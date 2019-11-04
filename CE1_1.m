a99313917a297f27c6630f1e78917b029fb74ec7
L1nky <linky.yersin@gmail.com>
2019-11-04 17:51:42 +0100

    Better 1.1

--- CE1_1.m
+++ CE1_1.m
@@ -3,21 +3,21 @@ T_e = 0.1;
 sys = tf([-1 3], [1 1.12 2]);
 sysd = c2d(sys, T_e);
 
-simin.time = T_e*(1:1000)';
+simin.time = T_e*(0:999)';
 
-simin.signals.values = [zeros(10,1); ones(990,1)];
+simin.signals.values = [zeros(10,1); ones(990,1)]/2;
 out = sim('CE1', 'StopTime', '99.9');
 
 hold on
-plot(out.simout/(2*pi*T_e))
+plot(simin.time - 1, out.simout.data*2, 'r')
 title("Step response of the system");
 step(sysd)
 
-simin.signals.values = [1; zeros(999,1)];
+simin.signals.values = [zeros(10,1); ones(10,1); zeros(980,1)]/2;
 out = sim('CE1', 'StopTime', '99.9');
 
 figure
 hold on
-plot(out.simout/(2*pi*T_e))
+plot(simin.time - 1.5, out.simout.data*2, 'r')
 title("Impulse response of the system");
 impulse(sysd)
