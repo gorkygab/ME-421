4f9b754f7480e2b5beb46f156fbae8f30d720262
GorkyGoub <42246534+gorkygab@users.noreply.github.com>
2019-11-05 03:45:26 +0100

    modified + added figures for 1_6

--- CE1_3.m
+++ CE1_3.m
@@ -9,12 +9,13 @@ U = toeplitz(simin.signals.values, [simin.signals.values(1) zeros(1, N-1)]);
 out = sim('CE1', 'StopTime', num2str((N - 1)*T_e));
 THETA = (U(:,1:K)'*U(:,1:K))\U(:,1:K)'*out.simout.data/T_e;
 
+%%
 hold on
 plot(simin.time(1:K), THETA, 'r')
-
+%%
 sys = tf([-1 3], [1 1.12 2]);
 sysd = c2d(sys, T_e);
 impulse(sysd, simin.time(1:K))
-
+%%
 true_theta = impulse(sysd, simin.time(1:K));
 error = sqrt(sum((THETA - true_theta).^2));
--- CE1_4.m
+++ CE1_4.m
@@ -10,7 +10,7 @@ out = sim('CE1', 'StopTime', num2str((N - 1)*T_e));
 y = out.simout.data;
 
 sys = tf([-1 3], [1 1.12 2]);
-sysd = c2d(sys, T_e);
+sysd = c2d(sys, T_e); %blue code
 true = impulse(sysd, simin.time(1:K));
 
 R_yu = intcor(y, u);
@@ -19,14 +19,14 @@ g = toeplitz(R_uu(1:K))\R_yu(1:K)/T_e;
 intcor_error = sqrt(sum((g - true).^2))
 
 hold on
-plot(simin.time(1:K), g, 'r')
+plot(simin.time(1:K), g, 'r') %intcor impulse response
 
 R_yu = xcorr(y, u);
 R_uu = xcorr(u, u);
 g = toeplitz(R_uu(N:N+K-1))\R_yu(N:N+K-1)/T_e;
 xcorr_error = sqrt(sum((g - true).^2))
 
-plot(simin.time(1:K), g, 'g')
+plot(simin.time(1:K), g, 'g') %xcorr impulse response
 impulse(sysd)
 
 legend('intcor', 'xcorr', 'true')
