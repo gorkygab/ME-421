cb1e770f5ca9ed31e5e4a35f6b6f1520e7a055f2
L1nky <linky.yersin@gmail.com>
2019-11-12 12:06:05 +0100

    Added 2.1.1 and 2.1.2

---
+++ CE-2/CE2_1_1.m
@@ -0,0 +1,26 @@
+load('laserbeamdataN.mat')
+N = length(u);
+f = 1000;
+t = (0:N-1)/f;
+
+m = 50;
+phi = toeplitz(zeros(m, 1), [0; u(1:end-1)])';
+
+theta_hat = (phi'*phi)\phi'*y;
+y_hat = phi*theta_hat;
+
+hold on
+plot(t, y, 'b')
+plot(t, y_hat, 'r')
+legend({'$y$', '$\hat{y}$'},'Interpreter','latex')
+xlabel('Time [s]')
+ylabel('Beam position [m]')
+
+J = sum((y - y_hat).^2, 1);
+
+sigma_2 = J/(N - m);
+cov_theta_hat = sigma_2*inv(phi'*phi);
+
+figure
+errorbar(theta_hat, 2*(diag(cov_theta_hat).^0.5))
+title('Impulse response')
---
+++ CE-2/CE2_1_2.m
@@ -0,0 +1,52 @@
+load('laserbeamdataN.mat')
+N = length(u);
+f = 1000;
+t = (0:N-1)/f;
+
+m = 2;
+n = 2;
+phi = [toeplitz(zeros(n, 1), [0; -y(1:end-1)]); toeplitz(zeros(m, 1), [0; u(1:end-1)])]';
+
+theta_hat = (phi'*phi)\phi'*y;
+y_hat = phi*theta_hat;
+
+hold on
+plot(t, y, 'b')
+plot(t, y_hat, 'r')
+legend({'$y$', '$\hat{y}$'},'Interpreter','latex')
+xlabel('Time [s]')
+ylabel('Beam position [m]')
+
+J = sum((y - y_hat).^2, 1);
+
+sys = tf(theta_hat(3:4)', [1 theta_hat(1:2)'], 1/f);
+y_m = lsim(sys, u, t);
+
+figure
+hold on
+plot(t, y, 'b')
+plot(t, y_m, 'r')
+legend({'$y$', '$\hat{y}$'},'Interpreter','latex')
+xlabel('Time [s]')
+ylabel('Beam position [m]')
+
+J_m = sum((y - y_m).^2, 1);
+
+y_hat_iv = y_m;
+for i = 1:10
+    phi_iv = [toeplitz(zeros(n, 1), [0; -y_hat_iv(1:end-1)]); toeplitz(zeros(m, 1), [0; u(1:end-1)])]';
+
+    theta_hat_iv = (phi_iv'*phi)\phi_iv'*y;
+    sys = tf(theta_hat_iv(3:4)', [1 theta_hat_iv(1:2)'], 1/f);
+    y_hat_iv = lsim(sys, u, t);
+end
+
+figure
+hold on
+plot(t, y, 'b')
+plot(t, y_hat_iv, 'r')
+legend({'$y$', '$\hat{y}$'},'Interpreter','latex')
+xlabel('Time [s]')
+ylabel('Beam position [m]')
+
+J_iv = sum((y - y_hat_iv).^2, 1);
